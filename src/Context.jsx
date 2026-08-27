03a45f7b663934696ebfcdc52f0affbc2339d721
cs-apple <dimitri.dietz93@gmail.com>
2026-08-26 21:06:16 -0400

    made the playing field

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,15 +1,32 @@
 import { Children, createContext, useContext, useState } from "react";
 import React from 'react'
 
+
+
 const GameContext = createContext();
+
+//establich game board 
+
 
 //STATES
 export function GameProvider({children}) {
   const[phase, setPhase] = useState("START")
+  const[score, setScore] = useState(0)
+  const[board, setBoard] = useState(new Array(9).fill(false))
 
+  //creat the play space
+
+//function 
+
+//reset board 
+
+function placeMole(board){
+  const random = Math.floor(Math.random()* board.length)
+  board[random] = true
+}
 
   //provide the context to export     
-  const value = {phase, setPhase};
+  const value = {phase, setPhase, score, setScore, board, setBoard};
   return (<GameContext.Provider value={value}>{children}</GameContext.Provider>)
 }
 
--- src/GamePlay.jsx
+++ src/GamePlay.jsx
@@ -2,11 +2,28 @@ import React from 'react'
 import { useGame } from './Context'
 
 export default function GamePlay() {
-  const {phase, setPhase} = useGame();
+  const {phase, setPhase, score, board} = useGame();
+
+  function CreateBoard(){
+ const gameBoard = board.map((hole, index)=>{
+    if(hole ===false){
+      return <li key = {index} className='hole'> </li>
+    } else {
+      return <li li key = {index} className="mole"> </li>
+    }
+  })
+  return gameBoard;
+}
+
   return (
     <>
     <h1>play game</h1>
+    <p>Score: <span>{score}</span></p>
+    <button onClick={()=>setPhase("START")}>restart</button>
     <button onClick={()=>setPhase("END")}>GO to end</button>
+    <div>
+      <ul><CreateBoard /></ul>
+    </div>
     </>
   )
 }
